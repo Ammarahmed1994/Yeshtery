f26d2166e54e7389c0a5ab72e90f3de7a42729ea
Ammar Ahmed <ammarahmed1994@hotmail.com>
2021-06-11 22:51:03 +0200

    added some change to nav3 and Breadcrumb

--- src/components/Navbars/nav3.component.js
+++ src/components/Navbars/nav3.component.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import { Navbar, Nav} from 'react-bootstrap';
+import { Navbar} from 'react-bootstrap';
 
 class Nav3 extends Component {
   render() {
     return (
-      <div className="navbar3 ">
-        <div className="container-fluid">
+      <div className="navbar3 container-fluid">
   <Navbar>
   <ul className="nav3Ul">
-  <li className="nav3Li">Men</li>
-  <li className="nav3Li">Women</li>
-  <li className="nav3Li">UniSex</li>
-  <li className="nav3Li">Kids</li>
-  <li className="nav3Li">Best Sellers</li>
-  <li className="nav3Li">New Arrivals</li>
-  <li className="nav3Li">Offers</li>
+  <li>Men</li>
+  <li>Women</li>
+  <li>UniSex</li>
+  <li>Kids</li>
+  <li>Best Sellers</li>
+  <li>New Arrivals</li>
+  <li>Offers</li>
 </ul>
   
   </Navbar>
   </div>
-      </div>
     );
   }
 }
--- src/components/body/breadCrumb.component.js
+++ src/components/body/breadCrumb.component.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
- 
+import { Breadcrumb } from 'react-bootstrap';
+
 class BreadCrumb extends Component {
   render() {
     return (
-      <div>
-        This is BreadCrumb.
-      </div>        
+      <div >
+        <Breadcrumb className="bread">
+          <Breadcrumb.Item active>Men</Breadcrumb.Item>
+          <Breadcrumb.Item active> CLothing </Breadcrumb.Item>
+          <Breadcrumb.Item active>Tops</Breadcrumb.Item>
+          <Breadcrumb.Item active>Adidas</Breadcrumb.Item>
+          <Breadcrumb.Item active>Adidas Black T-Shirt</Breadcrumb.Item>
+        </Breadcrumb>
+      </div>
     );
   }
 }
- 
+
 export default BreadCrumb;
