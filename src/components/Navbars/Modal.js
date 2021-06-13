0d7b811f3ab517c41ff190926b7b642fc80cf4ce
Ammar Ahmed <ammarahmed1994@hotmail.com>
2021-06-13 21:22:31 +0200

    updated nav2 and created the cart modal

---
+++ src/components/Navbars/Modal.js
@@ -0,0 +1,45 @@
+// import React from 'react';
+ 
+
+// const Modal = ({ show }) => {
+// 	const showHideClassName = show ? "modal display-block" : "modal display-none";
+  
+// 	return (
+// 	  <div className={showHideClassName}>
+	
+// 	<div class="modal right fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2">
+// 		<div class="modal-dialog" role="document">
+// 			<div class="modal-content">
+
+// 				<div class="modal-header">
+// 					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+// 					<h4 class="modal-title" id="myModalLabel2">Right Sidebar</h4>
+// 				</div>
+
+// 				<div class="modal-body">
+// 					<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+// 					</p>
+// 				</div>
+
+// 			</div>
+// 		</div>
+// 	</div>
+// 	  </div>
+// 	);
+//   };
+
+
+// export const Modal = ({ handleClose, show, children }) => {
+//   const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+//   return (
+//     <div className={showHideClassName}>
+//       <section className="modal-main">
+//         {children}
+//         <button type="button" onClick={handleClose}>
+//           Close
+//         </button>
+//       </section>
+//     </div>
+//   );
+// };
--- src/components/Navbars/nav2.component.js
+++ src/components/Navbars/nav2.component.js
@@ -3,32 +3,84 @@ import { faUser, faSearch, faShoppingCart, faHeart } from "@fortawesome/free-sol
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Row, Col } from 'react-bootstrap';
 import adidas from '../../images/adidas.svg';
-
+import { Button, Modal } from 'react-bootstrap';
 
 class Nav2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+  }
+
+  handleShow = () => {
+    this.setState({
+      show: true
+    })
+  }
+  handleClose = () => {
+    this.setState({
+      show: false
+    })
+  }
+
   render() {
     return (
       <div className="navbar2 container-fluid">
         <div>
           <Row>
             <Col className="margin-search">
-                <FontAwesomeIcon icon={faSearch} />
-                <input  type="text" placeholder="Search" />
+              <FontAwesomeIcon icon={faSearch} />
+              <input type="text" placeholder=" Search" />
             </Col>
 
             <Col >
-              <img src={adidas} alt="adidas"  height="70px"/>
+              <img src={adidas} alt="adidas" height="70px" />
             </Col>
             <Col className="fl nav2FontSize nav2Icons">
               <div >
-                <span className="paddingRight"><FontAwesomeIcon icon={faShoppingCart} size="lg"/><span className="badge badge-danger">3</span>{' '}Cart</span>
-                <span className="paddingRight"><FontAwesomeIcon icon={faHeart} size="lg"/>{' '}Wishlist </span>
-                <span className="paddingRight"><FontAwesomeIcon icon={faUser} size="lg"/>{' '} Login</span>
+                <span className="paddingRight" onClick={() => { this.handleShow() }}><FontAwesomeIcon icon={faShoppingCart} size="lg" /><span className="badge badge-danger">3</span>{' '}Cart</span>
+                <span className="paddingRight"><FontAwesomeIcon icon={faHeart} size="lg" />{' '}Wishlist </span>
+                <span className="paddingRight"><FontAwesomeIcon icon={faUser} size="lg" />{' '} Login</span>
               </div>
             </Col>
           </Row>
         </div>
+
+
+        <Modal show={this.state.show} className="cartModal">
+          <div onClick={()=>{this.handleClose()}}><span className="closeModal">X</span></div>
+          <h4>My Cart</h4>
+          <h6>Cart Summary</h6>
+          <table>
+            <tr>
+              <td rowspan="1"><img class="profilePicture" src="https://picsum.photos/id/1015/1000/600/" /></td>
+              <td> Lorem ipsum dolor sit amet<br /> consectetur adipiscing elit.<br />
+                <div className="cartQuantity">Quantity: 1</div>
+                <div className="row cartDetails">
+                  <div className="priceAfterSale">1,200 LE</div>
+                  <Button>Remove</Button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2" className="total">Total: 1,200 LE</td>
+            </tr>
+          </table>
+          <div className="row cartButtons">
+            <Button variant="warning">Review Cart</Button>
+            <Button>Complete Checkout</Button>
+          </div>
+        </Modal>
+
+        {/* <Modal show={this.state.show} className="cartModal">
+          <h4>My Cart</h4>
+          <h6>Cart Summary</h6>
+         Your cart is empty
+        </Modal> */}
+
       </div>
+
     );
   }
 }
