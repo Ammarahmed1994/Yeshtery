71a1b1bf41d127bba5d6717ca3a2497a88fb76a1
Ammar Ahmed <ammarahmed1994@hotmail.com>
2021-06-13 23:20:05 +0200

    updated similarProduct component

--- src/components/body/SimilarProducts.component.js
+++ src/components/body/SimilarProducts.component.js
@@ -12,7 +12,6 @@ class SimilarProducts extends Component {
   render() {
 
     const similarProducts = this.props.similarProducts;
-    console.log(`similar`, similarProducts)
     return (
       <div className="container-fluid similarProducts">
 
@@ -21,7 +20,6 @@ class SimilarProducts extends Component {
         <div className="similarProductsCard">
         {
           similarProducts.map((product, index)=> {
-            console.log('jflkdsaf',product);
             return(<Card key={index} style={{ width: '18rem' }}>
             <Card.Img variant="top" src={product.img} style={{ height:'150px' }}/>
             <Card.Body>
@@ -54,95 +52,6 @@ class SimilarProducts extends Component {
             
           })
         }
-     
-{/* 
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="https://picsum.photos/id/1015/1000/600/" style={{ height:'150px' }}/>
-  <Card.Body>
-    <Card.Text>
-      Some quick example text to build on the card title and make up 
-    </Card.Text>
-
-              <div className="priceAfterSale">1,200 LE</div>
-              <div className="row">
-              <div className="priceBeforeSale col-md-4">1420 LE</div>
-              <div className="salePercentage">30% Off</div>
-              <img src={adidas} alt="adidas" className="DetailsLogo" />
-            </div>
-            <div className="row stars">
-              <div >
-                <StarRatings
-                  rating={4.5}
-                  starRatedColor="orange"
-                  numberOfStars={5}
-                  name='rating'
-                  starDimension={17}
-                  starSpacing={1}
-                /></div>
-              <div className="rateOfFive">4.5 of 5</div>
-            </div>
-            <br />
-              <div className="pickupFrom">pickup from GnenaMall</div>
-  </Card.Body>
-</Card>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="https://picsum.photos/id/1015/1000/600/" style={{ height:'150px' }}/>
-  <Card.Body>
-    <Card.Text>
-      Some quick example text to build on the card title and make up 
-    </Card.Text>
-
-              <div className="priceAfterSale">1,200 LE</div>
-              <div className="row">
-              <div className="priceBeforeSale col-md-4">1420 LE</div>
-              <div className="salePercentage">30% Off</div>
-              <img src={adidas} alt="adidas" className="DetailsLogo" />
-            </div>
-            <div className="row stars">
-              <div >
-                <StarRatings
-                  rating={4.5}
-                  starRatedColor="orange"
-                  numberOfStars={5}
-                  name='rating'
-                  starDimension={17}
-                  starSpacing={1}
-                /></div>
-              <div className="rateOfFive">4.5 of 5</div>
-            </div>
-            <br />
-              <div className="pickupFrom">pickup from GnenaMall</div>
-  </Card.Body>
-</Card>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="https://picsum.photos/id/1015/1000/600/" style={{ height:'150px' }}/>
-  <Card.Body>
-    <Card.Text>
-      Some quick example text to build on the card title and make up 
-    </Card.Text>
-
-              <div className="priceAfterSale">1,200 LE</div>
-              <div className="row">
-              <div className="priceBeforeSale col-md-4">1420 LE</div>
-              <div className="salePercentage">30% Off</div>
-              <img src={adidas} alt="adidas" className="DetailsLogo" />
-            </div>
-            <div className="row stars">
-              <div >
-                <StarRatings
-                  rating={4.5}
-                  starRatedColor="orange"
-                  numberOfStars={5}
-                  name='rating'
-                  starDimension={17}
-                  starSpacing={1}
-                /></div>
-              <div className="rateOfFive">4.5 of 5</div>
-            </div>
-            <br />
-              <div className="pickupFrom">pickup from GnenaMall</div>
-  </Card.Body>
-</Card> */}
         </div>
 
       </div>        
